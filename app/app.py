d5eecd2632461d4b7944f8091f6c92eb93961106
German Lechner <glechner@frba.utn.edu.ar>
2023-11-26 18:01:10 -0300

    restablecer imagen

--- app/app.py
+++ app/app.py
@@ -34,7 +34,9 @@ def index():
 def estudiantes():
     return jsonify({'estudiantes': datos_participantes()})
 
-
+@app.route('/probando')
+def hola():
+    return 'hola!!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
